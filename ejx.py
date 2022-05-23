8e272492d9fcc97a82555b0f288219fd4b5f6989
MalenaLopezMuniz <malenalopez.mz@gmail.com>
2022-05-22 22:34:40 -0300

    subo guías

---
+++ ejx.py
@@ -0,0 +1,14 @@
+#Ejercicio 10
+#Escribí un programa que lea una cadena y devuelva un diccionario con la cantidad de apariciones de cada carácter en la cadena 
+# (considerar que las mayúsculas difieren de las minúsculas, por lo que, si el string es "Agua", el carácter "A" tiene 1 aparición 
+# y el carácter "a" también tiene 1).
+
+contadores = {}
+cadena = input("escribi una cadena:")
+for caracter in cadena:
+    if caracter in contadores:
+        contadores[caracter]+=1
+	contadores[caracter]=1
+
+for valor in contadores.keys():
+    print(valor, contadores[valor])
